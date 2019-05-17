28eb29406db6419fe60cd1d38ddbdece4bca8840
Alexandr Vanilchuk <he4then.mail@gmail.com>
2019-05-17 04:15:21 +0300

    Graphic method added

--- scripts/graphic.js
+++ scripts/graphic.js
@@ -355,25 +355,14 @@ function checkLineIntersection ( line1StartX, line1StartY, line1EndX, line1EndY,
   numerator2 = ( ( line1EndX - line1StartX ) * a ) - ( ( line1EndY - line1StartY ) * b );
   a = numerator1 / denominator;
   b = numerator2 / denominator;
-
-  // if we cast these lines infinitely in both directions, they intersect here:
   result.x = line1StartX + ( a * ( line1EndX - line1StartX ) );
   result.y = line1StartY + ( a * ( line1EndY - line1StartY ) );
-  /*
-   // it is worth noting that this should be the same as:
-   x = line2StartX + (b * (line2EndX - line2StartX));
-   y = line2StartX + (b * (line2EndY - line2StartY));
-   */
-  // if line1 is a segment and line2 is infinite, they intersect if:
   if ( a > 0 && a < 1 ) {
     result.onLine1 = true;
   }
-  // if line2 is a segment and line1 is infinite, they intersect if:
   if ( b > 0 && b < 1 ) {
     result.onLine2 = true;
   }
-  // if line1 and line2 are segments, they intersect if both of the above are
-  // true
   return result;
 };
 
